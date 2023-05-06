748e33538aad6af837619086b772b8fa8b0897d4
Эдуард <krav_4@mail.ru>
2023-05-06 11:16:13 +0300

    Создание динамической разметки - шаблонизация

--- src/main.js
+++ src/main.js
@@ -1,19 +1,31 @@
 /*Точка входа*/
 
 import {RenderPosition, render} from './render.js';
-import SortView from './view/sort-view.js';
-import FilterView from './view/filter-view.js';
 import TripInfoView from './view/trip-info-view.js';
-import WaypointListPresenter from './presenter/waypoint-presenter.js';
+import FilterView from './view/filter-view.js';
+import SortView from './view/sort-view.js';
+import DestinationsModel from './model/destinations-model.js';
+import WaypointsModel from './model/waypoints-model.js';
+import OffersModel from './model/offers-model.js';
+import WaypointsListPresenter from './presenter/waypoints-presenter.js';
 
 const tripInfoContainer = document.querySelector('.trip-main');
 const tripEventsContainer = document.querySelector('.trip-events');
 const filtersContainer = document.querySelector('.trip-controls__filters');
 
-const waypointListPresenter = new WaypointListPresenter({waypointListContainer: tripEventsContainer});
+const destinationsModel = new DestinationsModel();
+const waypointsModel = new WaypointsModel();
+const offersModel = new OffersModel();
+
+const waypointsListPresenter = new WaypointsListPresenter({
+  waypointsListContainer: tripEventsContainer,
+  destinationsModel,
+  waypointsModel,
+  offersModel,
+});
 
 render(new FilterView(), filtersContainer);
 render(new SortView(), tripEventsContainer);
 render(new TripInfoView(), tripInfoContainer, RenderPosition.AFTERBEGIN);
 
-waypointListPresenter.init();
+waypointsListPresenter.init();
